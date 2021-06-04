c281d4ab8435ec2084dc56261210d4ca44fac480
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-06-04 15:22:35 +0300

    --add test

--- src/main/java/org/closure/gcp/GcpApplication.java
+++ src/main/java/org/closure/gcp/GcpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GcpApplication {
-//TEST
+//TES
 	public static void main(String[] args) {
 			SpringApplication.run(GcpApplication.class, args);
 		
